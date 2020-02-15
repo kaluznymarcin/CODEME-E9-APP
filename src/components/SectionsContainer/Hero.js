72798ee7e40e84dd7816308cd0ed350d9eb10e7c
Paweł Nieczuja-Ostrowski <pawel.nieczuja@onet.eu>
2020-02-15 14:35:17 +0100

    Add Content.js

--- src/components/SectionsContainer/Hero.js
+++ src/components/SectionsContainer/Hero.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import Content from './Content';
 
 export default (props) => (
   <section className="hero">
     <h1 className="listsTitle">{props.children}</h1>
+    <Content />
   </section>
 );
