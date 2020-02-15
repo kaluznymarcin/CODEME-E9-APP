16a867d429fcf76b58cee07e881e011d164e17a5
Henrik Kiepe <hkiepe@inkontor.com>
2020-02-15 15:23:02 +0100

    OK lista

--- src/components/Importer.js
+++ src/components/Importer.js
@@ -1,12 +1,11 @@
 import React from 'react';
- import Funkcja from '/.Utis' 
 
  const UNSPLASH_BASE_URL = "https://api.unsplash.com/collections?page=1&per_page=10&order_by=latest&client_id=";
  const UNSPLASH_ACCESS_KEY = "3f664ef5e4273eb908008a62f5bb36a44010ddbd641644700adac1dcd8a48e49";
 
 class Importer extends React.Component {
   state = {
-    imgs: [{}]
+    imgs: []
   }
 
   componentDidMount() {
@@ -19,16 +18,16 @@ class Importer extends React.Component {
 
 
       render() {
-        console.log(this.state.imgs[0].cover_photo);
+        console.log(this.state.imgs)
          return  ( 
           
         <div>
         <ul>
-         {/* <li> {this.state.imgs[0].cover_photo.urls.thumb}</li> */}
-        </ul>
+          {this.state.imgs.map(value => <li><img key={value.id} src={value.cover_photo.urls.thumb}></img></li>)}
+          </ul>
         </div>
         );
       }
     }
     
-    export default Importer; 
+    export default Importer;
