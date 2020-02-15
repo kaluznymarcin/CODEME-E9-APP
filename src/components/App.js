f9f7f397e318ef54bd1e1d2e8fc3f9b60891857a
Kuba <qbabialek@gmail.com>
2020-02-15 16:19:24 +0100

    header

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,34 @@
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Switch
-// } from 'react-router-dom';
-//  import './App.css'; 
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import './App.css';
 
-// import Header from './Header';
-// import Nav from './Nav';
-// import HomePage from './HomePage';
+import Header from './Header';
+import Nav from './Nav';
+import HomePage from './HomePage';
 
 import Importer from './Importer'
 
 function App() {
   return (
+    <Router>
+      <Header>Moja Aplikacja</Header>
     
+      <Nav />
       <Importer />
+      <Switch>
+        
+        {/* <Route path="/" component={HomePage} /> */}
+        <Route >
+          <HomePage />
+        </Route>
+      </Switch>
+    </Router>
+    
+      
   );
 }
 
