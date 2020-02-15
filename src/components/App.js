8bf9ad8b1b1cddbabe15117637305a7fcff3981b
Kuba <qbabialek@gmail.com>
2020-02-15 16:07:31 +0100

    powrot

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
- import './App.css'; 
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Switch
+// } from 'react-router-dom';
+//  import './App.css'; 
 
-import Header from './Header';
-import Nav from './Nav';
-import HomePage from './HomePage';
+// import Header from './Header';
+// import Nav from './Nav';
+// import HomePage from './HomePage';
 
 import Importer from './Importer'
 
 function App() {
   return (
-    <Header>Moja Aplikacja</Header>
     
-       <Nav />
       <Importer />
   );
 }
--- src/components/Header/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Title from './Title';
-
-export default (props) => (
-  <header className="pageHeader">
-    <Title>{props.children}</Title>
-  </header>
-);
