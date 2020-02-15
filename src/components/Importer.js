5f02870b19b94ade4d7ad8899f33c2eddda15216
Kuba <qbabialek@gmail.com>
2020-02-15 15:05:21 +0100

    funkcja

--- src/components/Importer.js
+++ src/components/Importer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-  
+ import Funkcja from '/.Utis' 
 
  const UNSPLASH_BASE_URL = "https://api.unsplash.com/collections?page=1&per_page=10&order_by=latest&client_id=";
  const UNSPLASH_ACCESS_KEY = "3f664ef5e4273eb908008a62f5bb36a44010ddbd641644700adac1dcd8a48e49";
@@ -19,13 +19,13 @@ class Importer extends React.Component {
 
 
       render() {
-        console.log(this.state.imgs);
+        console.log(this.state.imgs[0].cover_photo);
          return  ( 
           
         <div>
-      {/*   <ul>
-          {this.state.imgs.map(value => typeof value.urls.thumb !== undefined ? {value.urls.thumb} : undefined)}
-        </ul> */}
+        <ul>
+         {/* <li> {this.state.imgs[0].cover_photo.urls.thumb}</li> */}
+        </ul>
         </div>
         );
       }
