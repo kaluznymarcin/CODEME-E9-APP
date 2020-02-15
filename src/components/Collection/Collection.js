85e0ba676e8ec7df819896a944a26beaeb0c54ce
Michal <m.lytkowski@gmail.com>
2020-02-15 17:17:42 +0100

    Collection page render

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -1,57 +1,60 @@
 import React from 'react';
 
 import {UNSPLASH_ACCESS_KEY} from '../../constants';
+//import PropTypes from "prop-types";
+import { withRouter } from "react-router-dom";
+
 import './Collection.css';
-import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Link
-} from 'react-router-dom'
-const UNSPLASH_URL = `https://api.unsplash.com/photos/?client_id=${UNSPLASH_ACCESS_KEY}&page=1&per_page=10&order_by=latest`
 
 class Collection extends React.Component {
     state = {
-        apiData: null
+        collectionID :this.props.match.params.slug,
+        apiData: null,
     }
+
+
 
     componentDidMount() {
 
-        console.log('XXXXXXX');
+        const UNSPLASH_URL = `https://api.unsplash.com/photos/?client_id=${UNSPLASH_ACCESS_KEY}&collection=${this.collectionID}&page=1&per_page=100&order_by=latest`
+
+        fetch(UNSPLASH_URL)
+            .then(res => res.json())
+            .then(data => {
+                return this.setState({
+                    apiData: data})});
+
     }
 
 
-    // createList = () => {
-    //
-    //     let content  = [];
-    //
-    //     if(this.state.apiData)
-    //     {
-    //         console.dir(this.state.apiData)
-    //
-    //         const values = this.state.apiData;
-    //         values.map(function(item){
-    //
-    //             const {title, id, cover_photo} = item;
-    //            // console.log(cover_photo)
-    //             content.push(<div className='unsplash__item' key={id.toString()} data-id={id}>
-    //                 <Link to={id}><img src={cover_photo.urls.small} /><span>{title}</span>
-    //                 </Link>
-    //             </div>);
-    //         })
-    //     }
-    //
-    //     return content;
-    // }
+    createList = () => {
+
+        let content  = [];
+
+        if(this.state.apiData)
+        {
+
+            const values = this.state.apiData;
+            values.map(function(item){
+
+                const { id, urls} = item;
+
+                content.push(<div className='unsplash__item' key={id.toString()}>
+                    <img src={urls.small} />
+                </div>);
+            })
+        }
+
+        return content;
+    }
 
     render() {
 
-
-        return (
-            <div>HEllo world</div>
+        return  (
+            <div className="unsplash__gallery">{this.createList()}</div>
         )
     }
 }
 
 
-export default Collection;
+export default withRouter(Collection);
