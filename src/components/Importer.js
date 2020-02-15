167c9f84259d87f029886671435d60693b57d804
Kuba <qbabialek@gmail.com>
2020-02-15 14:50:32 +0100

    api

--- src/components/Importer.js
+++ src/components/Importer.js
@@ -1,7 +1,7 @@
 import React from 'react';
   
 
- const UNSPLASH_BASE_URL = "https://api.unsplash.com/photos/?client_id=";
+ const UNSPLASH_BASE_URL = "https://api.unsplash.com/collections?page=1&per_page=10&order_by=latest&client_id=";
  const UNSPLASH_ACCESS_KEY = "3f664ef5e4273eb908008a62f5bb36a44010ddbd641644700adac1dcd8a48e49";
 
 class Importer extends React.Component {
@@ -19,11 +19,13 @@ class Importer extends React.Component {
 
 
       render() {
+        console.log(this.state.imgs);
          return  ( 
+          
         <div>
-        <ul>
-          {this.state.imgs.map(value => value.urls.thumb && 'object' === typeof value.urls.thumb ? <li>{value.urls.thumb}</li> : undefined)}
-        </ul>
+      {/*   <ul>
+          {this.state.imgs.map(value => typeof value.urls.thumb !== undefined ? {value.urls.thumb} : undefined)}
+        </ul> */}
         </div>
         );
       }
