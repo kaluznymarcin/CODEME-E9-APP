380eb4c06762b0341cc8ca0a06105fd40e878c2f
Marcin Kałużny <marcin.kaluzny@codeme.pl>
2020-02-15 16:58:35 +0100

    sections

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -3,22 +3,26 @@ import fetchSections from '../../api/fetchSections';
 
 import './style.css';
 
+const list_creator = data => ((Array.isArray(data) && data) || []).map(
+  ({ id, title, total_photos, cover_photo: { urls: { thumb: image  } = {} } = {} }, index) => (
+    <div key={`s-${index}-${id}`}>
+      {image && <img src={image} alt="" />}
+    </div>
+  )
+);
+
 export default class extends React.Component {
   state = {
     data: null
   }
 
-  constructor(props) {
-    super(props);
-  }
-
   componentDidMount() {
-    fetchSections(data => this.setState({ data }));
+    fetchSections(data => { console.dir(data[0]); return this.setState({ data }); });
   }
 
   render() {
     return <div className="sections">
-      {JSON.stringify(this.state.data)}
+      {list_creator(this.state.data)}
     </div>
   }
 }
