80ed2dbe7b0d17549d2f601ec96fd0916392c661
Henrik Kiepe <hkiepe@inkontor.com>
2020-02-17 14:10:41 +0100

    I also added the react-router-dom through npm to the project and wrapped the components with the router. in the Navbar i use the Link function form react to not send a request to the server every time a user clicks a link in the navbar so that the server has to load the page every time

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = () => {
     return (
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <Link to="/" className="brand-logo">GRUPA_3 - APP</Link>
+                <Link to="/" className="brand-logo">GRUPA_3 - App</Link>
                 <ul className="right">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
